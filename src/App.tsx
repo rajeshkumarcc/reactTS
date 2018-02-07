2fd6b7587bdb8e005ca77cae104de80dfabd1e0a
Milan Savaliya <milan.savaliya@causecode.com>
2018-02-07 10:57:57 +0530

    Added unit test cases

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,9 @@ class App extends React.Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          <Hello name="Typescript" enthusiasmLevel={10} />
-        </p>
+        <div className="App-intro">
+          <Hello name="Typescript" enthusiasmLevel={3} />
+        </div>
       </div>
     );
   }
